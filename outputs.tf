fffb32a2ea58460b061d9744a00eaf50a8986fde
Oliver Borchert <borchero@icloud.com>
2020-08-02 02:25:43 +0200

    Add output variable for cron policy

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "policy_api" {
   value       = vault_policy.api.name
   description = "The name of the policy that should be assigned to Meerkat's API component."
 }
+
+output "policy_cron" {
+  value       = vault_policy.vpn_cron.name
+  description = "The name of the policy that should be assigned to Meerkat's Cron component."
+}
